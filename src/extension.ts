638e31392bbcdfc9f8dc0e39dd5320ce9e66f51b
JavierPrior845 <javierprior04@gmail.com>
2026-06-22 16:37:51 +0200

    chore: init project

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('clean-ast-linter.helloWorld', () => {
+        vscode.window.showInformationMessage('Clean AST Linter is now active!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
