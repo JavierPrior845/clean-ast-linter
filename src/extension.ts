14fe5bd8af3813efff51d7c2e584aa9638445917
JavierPrior845 <javierprior04@gmail.com>
2026-06-23 10:14:29 +0200

    chore: pre-commit validations

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('clean-ast-linter.helloWorld', () => {
+    const disposable = vscode.commands.registerCommand('clean-ast-linter.helloWorld', () => {
         vscode.window.showInformationMessage('Clean AST Linter is now active!');
     });
 
